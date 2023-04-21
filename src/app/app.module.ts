9912362cdcc1ea63b0f12bc293e41e5b78570f9b
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-04-21 17:09:24 -0400

    Revert "init"
    This reverts commit f36c8024eabfe5b21d1f2fe7468ae7a2535dfa7f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { VehiclesComponent } from './components/vehicles/vehicles.component';
-import { BrandsComponent } from './components/brands/brands.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    VehiclesComponent,
-    BrandsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
