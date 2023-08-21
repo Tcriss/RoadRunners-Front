bc46e6463216e62db87d4e4a4c24a654533e25da
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-08-21 15:15:49 -0400

    fixes

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -2,15 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Api } from '../../interfaces/api';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  url:string;
+  url: string;
 
   constructor(public http:HttpClient) {
-    this.url = 'https://roadrunners-backend.onrender.com/';
+    this.url = environment.api_url;
   }
 
   async show(): Promise<Observable<Api[]>> {
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    api_url: 'https://roadrunners-backend.onrender.com/'
+};
