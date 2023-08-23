32559a6c6ba75ee5192366e98ff804d7212fa89e
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-08-23 19:04:32 -0400

    fixes

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -2,14 +2,14 @@ import { HttpClient, HttpHeaders, HttpParamsOptions } from '@angular/common/http
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Api } from '../../interfaces/api';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  url: string = environment.api_url;
+  url: string = environment.url;
   key: string = environment.key;
   headers: HttpHeaders = new HttpHeaders().set('API_KEY', this.key);
 
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    url: 'https://pruebas-gnsi.onrender.com/',
+    key: 'E3AAoak5Sr1xDC6I925sneMtjy6LetjmZ8LeDKMwwAhkmNmgECD8SHn1OdUnmaTR6v8YOHgOLCODoc3kLCsEgCPVqrJn5b4Svvt1J9GVBGieoQtt8l21ImExd5RaAgse'
+};
