ddbb08bd0864aa85ea1eb88aad75c9736dd7ab9e
Cristian Tejeda <do.cristian.tr@hotmail.com>
2024-01-30 16:45:12 -0400

    fixes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,12 @@
 export const environment = {
-    production: false,
-    url: 'http://localhost:3000/',
-    // put your auth0 congi in order to it work properly
-    config: {
-      domain: '',
-      clientId: '',
-      audience: '',
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
+  production: false,
+  url: 'http://localhost:3000/',
+  config: {
+    domain: '',
+    clientId: '',
+    audience: '',
+    authorizationParams: {
+      redirect_uri: window.location.origin
     }
-};
+  }
+}
