7c0c9a9634f183638726319ff60c033fd8bc6bdb
Cristian Tejeda <do.cristian.tr@hotmail.com>
2024-03-26 21:57:42 -0400

    feat: new back connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    url: 'https://roadrunners-backend.onrender.com/',
+    url: 'https://roadrunners-api.onrender.com/',
     config: {
         domain: 'dev-3f45fsqiwdpfl2ds.us.auth0.com',
         clientId: 'DIgl0vPzUoUVwVlk0OGZcRz37bJo8uCq',
